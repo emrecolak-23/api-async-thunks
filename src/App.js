4acefb16212a4af670f254b16091701a59ee9e69
Emre Çolak <emrecolak@emrecolak.local>
2023-01-13 22:20:12 +0300

    creating the store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import UsersList from './components/UsersList';
+
 function App() {
-  return <div className="text-3xl">App</div>;
+  return (
+    <div className="container mx-auto">
+      <UsersList />
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { store } from './store';
+import { Provider } from 'react-redux';
+
 import App from './App';
 
 const el = document.getElementById('root');
 
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
