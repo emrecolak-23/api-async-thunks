9c4dfbaa513e07992f4e6eb5d48a6331d429b92d
Emre Çolak <emrecolak@emrecolak.local>
2023-01-15 14:57:14 +0300

    creating fetch aware button

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -47,11 +47,11 @@ function UsersList() {
     <div>
       <div className="flex flex-row justify-between m-2">
         <h1 className="m-2 text-xl">Users</h1>
-        {isCreatingUser ? (
-          'Creating User...'
-        ) : (
-          <Button onClick={handleAddUser}>+ Add User</Button>
-        )}
+
+        <Button loading={isCreatingUser} onClick={handleAddUser}>
+          + Add User
+        </Button>
+
         {creatingUserError && 'Error Creating User...'}
       </div>
       {renderedUsers}
